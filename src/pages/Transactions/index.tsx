8f693eff0e685ee85f7a569eafbfdd2947ec410b
Luis Paulo Ruda <luispauloruda@gmail.com>
2022-09-21 23:17:26 -0300

    feature: created table of transactions

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,10 +1,38 @@
 import { Header, Summary } from '../../components'
+import {
+  TransactionsContainer,
+  TransactionsTable,
+  PriceHighlight,
+} from './styles'
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Notebook novo</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 4.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>11/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
