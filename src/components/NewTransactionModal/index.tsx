3e4e3994ad6870ccfdf9545d6b7114aa0fafa4ea
Luis Paulo Ruda <luispauloruda@gmail.com>
2022-09-22 18:02:49 -0300

    feature: added transaction type buttons

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,14 @@
 import * as Dialog from '@radix-ui/react-dialog'
-import { X } from 'phosphor-react'
+// import * as RadioGroup from '@radix-ui/react-radio-group'
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
 
-import { Overlay, Content, CloseButton } from './styles'
+import {
+  Overlay,
+  Content,
+  CloseButton,
+  TransactionType,
+  TransactionTypeButton,
+} from './styles'
 
 export function NewTransactionModal() {
   return (
@@ -15,9 +22,20 @@ export function NewTransactionModal() {
         </CloseButton>
 
         <form action="">
-          <input type="text" placeholder="Descrição" required />
-          <input type="number" placeholder="Preço" required />
-          <input type="text" placeholder="Categoria" required />
+          <input type="text" placeholder="Descrição" />
+          <input type="number" placeholder="Preço" />
+          <input type="text" placeholder="Categoria" />
+
+          <TransactionType>
+            <TransactionTypeButton variant="income" value="income">
+              <ArrowCircleUp size={24} />
+              Entrada
+            </TransactionTypeButton>
+            <TransactionTypeButton variant="outcome" value="outcome">
+              <ArrowCircleDown size={24} />
+              Saída
+            </TransactionTypeButton>
+          </TransactionType>
 
           <button type="submit">Cadastrar</button>
         </form>
